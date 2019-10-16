ae48ab784877a3c054b2db6055744b274f5d5ef5
nranmag <43456295+nranmag@users.noreply.github.com>
2019-10-15 22:30:34 -0500

    Update sentencia.sql

--- sentencia.sql
+++ sentencia.sql
@@ -1,3 +1,15 @@
 Select * from mfs2p010g.fcsptl10 where TLPPRL LIKE 'ZM%'
 
 https://www.youtube.com/watch?v=sSZT616qXvY
+
+Type of host iSeries
+
+Interface LAN
+
+Host Name QRYPROD.AHE.POK.IBM.COM
+
+Port 992
+
+User nrangel
+
+passw Nahum83Rangel
